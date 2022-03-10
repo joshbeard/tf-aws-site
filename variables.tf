d5c7413f006cfe10beb4fdd1a91fb17a391fd7dc
Josh Beard <jbeard@contractor.usgs.gov>
2022-03-09 23:06:05 -0700

    Add undeclared var

--- variables.tf
+++ variables.tf
@@ -9,6 +9,11 @@ variable "domain" {
   type    = string
 }
 
+variable "iam_name" {
+  description = "The name to give IAM resources"
+  type        = string
+}
+
 variable "subject_alternative_names" {
   description = "Additional subject_alternative_names to add to the certificate request (ACM). By default, *.domain is added."
   type        = list
