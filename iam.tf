439667cfbc4c70754fdda48edbde4cddfa6ab0be
Josh Beard <jbeard@contractor.usgs.gov>
2022-03-09 23:04:27 -0700

    Fix formatting

--- iam.tf
+++ iam.tf
@@ -5,19 +5,19 @@ data "aws_iam_policy_document" "site_deploy" {
       "s3:DeleteObject",
       "s3:ListBucket",
       "s3:PutObject",
-      "s3:PutObjectAcl"
-      "s3:PutObjectVersionAcl",
+      "s3:PutObjectAcl",
+      "s3:PutObjectVersionAcl"
     ]
     resources = [
       "arn:aws:s3:::${var.bucket_name}/*",
-      "arn:aws:s3:::${var.bucket_name}",
+      "arn:aws:s3:::${var.bucket_name}"
     ]
   }
 
   statement {
     sid     = "DeployWebsiteCloudfront"
     actions = [
-      "cloudfront:CreateInvalidation",
+      "cloudfront:CreateInvalidation"
     ]
     resources = [aws_cloudfront_distribution.site_distribution.arn]
   }
