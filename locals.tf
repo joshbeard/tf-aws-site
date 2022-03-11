8975ecb6edec7225626767b608cf27c864385195
Josh Beard <josh@joshbeard.me>
2022-03-11 10:55:08 -0700

    Fix variable reference

--- locals.tf
+++ locals.tf
@@ -1,7 +1,10 @@
+# ------------------------------------------------------------------------------
+# Local variables
+# ------------------------------------------------------------------------------
 locals {
-  hyphen_domain_name = replace(domain, ".", "-")
+  hyphen_domain_name = replace(var.domain, ".", "-")
 
-  bucket_name     = var.bucket_name != null ? var.bucket_name : "s3-website-${var.domain}"
+  bucket_name     = var.bucket_name != null ? var.bucket_name : "s3-website-${local.hyphen_domain_name}"
   log_bucket_name = var.log_bucket_name != null ? var.log_bucket_name : "${local.bucket_name}-logs"
   iam_name        = var.iam_name != null ? var.iam_name : "s3-deployer-${local.hyphen_domain_name}"
   tags            = merge(var.tags, { "domain" = var.domain })
