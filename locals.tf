8bc3e08b4f0eb9c1b4ee97b31ff2cb835f311f3b
Josh Beard <josh@joshbeard.me>
2022-03-14 20:57:40 -0600

    tags: add '_created_by' tag
    Add a "_created_by = terraform" tag to resources.

--- locals.tf
+++ locals.tf
@@ -7,5 +7,5 @@ locals {
   bucket_name     = var.bucket_name != null ? var.bucket_name : "s3-website-${local.hyphen_domain_name}"
   log_bucket_name = var.log_bucket_name != null ? var.log_bucket_name : "${local.bucket_name}-logs"
   iam_name        = var.iam_name != null ? var.iam_name : "s3-deployer-${local.hyphen_domain_name}"
-  tags            = merge(var.tags, { "domain" = var.domain })
+  tags            = merge(var.tags, { "domain" = var.domain, "_created_by" = "terraform" })
 }
